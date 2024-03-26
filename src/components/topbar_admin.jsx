5f8942dc9ee8d6facc538cc2bfecb94dd713f2d6
vincent216523 <1155159701@link.cuhk.edu.hk>
2024-03-26 21:47:11 +0800

    topbar_admin

--- src/components/topbar_admin.jsx
+++ src/components/topbar_admin.jsx
@@ -1,48 +1,69 @@
 import ReactDOM from 'react-dom/client';
 import React, { Component, useState } from 'react';
 import './topbar_admin.css'
-import { Home, PeopleOutline, ChatbubbleOutline, SearchOutline, Create, CreateOutline, Heart, HeartOutline, Person, PersonOutline, Menu} from 'react-ionicons'
+import { Home, People, Chatbubble, PeopleOutline, ChatbubbleOutline, SearchOutline, Create, CreateOutline, Heart, HeartOutline, Person, PersonOutline, Menu} from 'react-ionicons'
 import ChipichpiLogo from './ChipichpiLogo.png';
+import { useLocation, Link ,withRouter} from 'react-router-dom';
 
-class Topbar_admin extends React.Component {
+const Topbar_admin = () => {
+    const location = useLocation()
+    return <Topbar_admin_component location={location} />
+  }
 
+class Topbar_admin_component extends React.Component {
     render(){
-        function turnOnBackground(e) {
-            e.target.style.background = 'rgba(100, 100, 100, 0.5)';
-        }
-        
-        function turnOffBackground(e) {
-            e.target.style.background = 'rgba(0, 0, 0, 0)';
-        }
-        
-        function changecolor(e) {
-            if (e.target.style.color == '#555555'){
-                e.target.style.color = '#ffffff';
-            } else {
-                e.target.style.color = '#555555';
-            }
-        }
-
-
-
+        const { location } = this.props;
     return(
         <div className="topbarContainer_admin">
             <div className="topbaricon_admin">
-                <a href='/' >
-                    <button className="topbarLogo" style={{ backgroundImage: `url(${ChipichpiLogo})`, display: 'flex', alignItems: 'center',justifyContent: 'center', backgroundSize: 'cover',backgroundPosition: 'center', height: '40px'}}></button>
-                </a>
+            <Link to="/">
+            <button
+              className="topbarLogo"
+              style={{
+                backgroundImage: `url(${ChipichpiLogo})`
+              }}
+            ></button>
+            </Link>
             </div>
             <div>
                 <div className="topbarmiddle_admin" style={{alignContent: 'center'}}>
-                    <button className="topbarIconItem_admin" onClick={(e) => {e.preventDefault(); window.location.href='/admin_manage_user';changecolor(e);}} onMouseEnter={turnOnBackground} onMouseLeave={turnOffBackground} >
-                        <PeopleOutline color={'#555555n'} background={'rgba(0, 0, 0, 0)'} height="30px" width="30px" />
-                    </button>
-                    <button className="topbarIconItem_admin" onClick={(e) => {e.preventDefault(); window.location.href='/admin_manage_post';changecolor(e);}} onMouseEnter={turnOnBackground} onMouseLeave={turnOffBackground} >
-                        <ChatbubbleOutline color={'#ffffff'} background={'rgba(0, 0, 0, 0)'} height="30px" width="30px"/>
-                    </button>
+                <Link to='/admin_manage_post' className="topbarIconItem">
+                {location.pathname.includes('/admin_manage_post') ? (
+                    <People
+                    color="#ffffff"
+                    background="transparent"
+                    height="30px"
+                    width="30px"
+                    />
+                ) : (
+                    <PeopleOutline
+                    color="#555555"
+                    background="transparent"
+                    height="30px"
+                    width="30px"
+                    />
+                )}
+            </Link>
+            <Link to='/admin_manage_user' className="topbarIconItem">
+                {location.pathname.includes('/admin_manage_user') ? (
+                    <Chatbubble
+                    color="#ffffff"
+                    background="transparent"
+                    height="30px"
+                    width="30px"
+                    />
+                ) : (
+                    <ChatbubbleOutline
+                    color="#555555"
+                    background="transparent"
+                    height="30px"
+                    width="30px"
+                    />
+                )}
+            </Link>
                 </div>
             </div>
         </div>
                 )}}
 
-export default Topbar_admin
+export default Topbar_admin;
