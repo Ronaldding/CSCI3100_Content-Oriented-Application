6f2821274b22d5d5e700e4f67fb50b22b48deb1f
vincent216523 <1155159701@link.cuhk.edu.hk>
2024-04-09 02:05:12 +0800

    admin

--- src/Register_page.jsx
+++ src/Register_page.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import login_background from './background3.jpg';
 import './style.css';
 import { PersonCircle, LockClosedOutline, MailOutline } from 'react-ionicons'
+import Popup from 'reactjs-popup';
+import './admin_page/user.css'
 
 class Register_page extends React.Component {
     render(){
@@ -18,38 +20,73 @@ class Register_page extends React.Component {
 class Register_block extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {show_message:false, message: "hello"}
+        this.Register = this.Register.bind(this)
     }
 
-    async login_handleSubmit(event){
+    Register =  async(event) =>{
         event.preventDefault();
+        const email = document.getElementById('email').value;
+        const login_username =  document.getElementById('login_username').value;
+        const password =  document.getElementById('password').value;
+        const data ={
+            username: login_username,
+            email: email,
+            password: password
+          }
+          console.log(data)
+        const response = await fetch(`http://localhost:8800/register`, { 
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+         })
+         .then(response => 
+            {
+                if(response.status == 400){
+                    let message = response.message;
+                    console.log(message)
+                    this.setState({show_message:true})
+                    setTimeout(() => {
+                        this.setState({ show_message: false });},5000)
+                    this.setState({message: message})
+                } else if (response.status == 200) {
+                    let message = "Register successful!"
+                    console.log(message)
+                    this.setState({show_message:true})
+                    this.setState({message: message})
+                }
+            })
     }
     render() {
         return (
             <div className="register_container" style={{justifyContent: 'center', alignItems: 'center'}}>
                 <div className='register_form'>
                     <h3 className='logo'>Register</h3>
-                    <form onSubmit={this.login_handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
+                    <form onSubmit={this.Register} style={{display: 'flex', flexDirection: 'column'}}>
                         <div className="input_container">
                             <span className="icon">
                                 <MailOutline color={'#ffffff'}  />
                             </span>
-                            <input type="email" required/>
-                            <label>Email</label>
+                            <input type="email" id="email" name="email" required/>
+                            <label for="email">Email</label>
                         </div>
                         <div className='input_container '>
                             <span class='icon'>
                             <PersonCircle color={'#ffffff'}   />
                             </span>
-                            <input type='text' id='login_username' required/>
-                            <label>Username</label>
+                            <input type='text' id='login_username' name="login_username" required/>
+                            <label for="login_username">Username</label>
                         </div>
                         <div className="input_container">
                             <span className="icon">
                                 <LockClosedOutline color={'#ffffff'}  />
                             </span>
-                            <input type="password" required/>
-                            <label>Password</label>
+                            <input type="password" id='password' name='password' required/>
+                            <label for="password">Password</label>
                         </div>
+                        {this.state.show_message &&<h5 id='message'>{this.state.message}</h5>}
                         <button type='submit' className="button">Register</button>
                         <div className="register" style={{justifyContent: 'center', alignContent: 'center', display: 'flex'}}>
                             <p>Already have an account? <a href="/login" className="register_link">Login</a></p>
@@ -60,6 +97,7 @@ class Register_block extends React.Component {
                 </div>
 
             </div>
+            
         )}
 }
 
---
+++ src/admin_page/edit_user.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './edituser.css';
+
+const Edit_User = () => {
+    return(
+        <div className="popup_block">
+        <div className="modal">
+            <button className="close">
+                &time;
+                </button>
+                <div className="title">
+                    <h4>Update user:</h4>
+                </div>
+                <div className="block">
+                    <h4>Update user:</h4>
+                </div>
+        </div>
+        </div>
+    )
+}
+
+export default Edit_User;
