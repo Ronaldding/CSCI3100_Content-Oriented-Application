f930496095d3bd09c2de5686cbeb76d8c27edb5c
jeffrey0630 <jeffreytai901@hotmail.com>
2024-04-10 13:09:32 +0800

    Updated storing image

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,216 +1,259 @@
-const router = require("express").Router();
-const Post = require("../models/Post");
-const User = require("../models/User");
-const cors = require('cors');
-router.use(cors());
+import { v2 as cloudinary } from 'cloudinary'
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+})
+const router = require('express').Router()
+const Post = require('../models/Post')
+const User = require('../models/User')
+const cors = require('cors')
+router.use(cors())
+
 //create a post
+router.post('/post', async (req, res) => {
+  const userId = req.body.userId
+  const imgURL = req.body.img
 
-router.post("/post", async (req, res) => {
-  const newPost = new Post(req.body);
+  const random = Date.now()
+  const imageURL = await cloudinary.uploader.upload(imgURL[0], {
+    upload_preset: 'unsigned_upload_posts',
+    public_id: `${userId}_${random}`,
+    allowed_formats: ['jpg', 'jpeg', 'png', 'svg', 'ico', 'webp'],
+  })
+
+  const newPost = new Post({
+    userId: userId,
+    username: req.body.username,
+    desc: req.body.desc,
+    img: imageURL,
+    video: req.body.video,
+    likes: [],
+    isHide: false,
+    tags: req.body.tags,
+    comments: [],
+    retweets: [],
+  })
   try {
-    const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
+    const savedPost = await newPost.save()
+    res.status(200).json(savedPost)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-//update a post
+})
 
-router.put("/post/:id", async (req, res) => {
+//update a post
+router.put('/post/:id', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
-    const user = await User.findById(req.body.userId);
-    if (post.userId === req.body.userId|| user.isAdmin) {//owner of the post
-      await post.updateOne({ $set: req.body });
-      res.status(200).json("Post has been updated");
+    const post = await Post.findById(req.params.id)
+    const user = await User.findById(req.body.userId)
+    if (post.userId === req.body.userId || user.isAdmin) {
+      //owner of the post
+      await post.updateOne({ $set: req.body })
+      res.status(200).json('Post has been updated')
     } else {
-      res.status(403).json("You can update only your post");
+      res.status(403).json('You can update only your post')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-
+})
 
 //delete a post
-router.delete("/post/:id", async (req, res) => {
+router.delete('/post/:id', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
-    const user = await User.findById(req.body.userId);
+    const post = await Post.findById(req.params.id)
+    const user = await User.findById(req.body.userId)
 
     if (!post) {
-      return res.status(404).json("Post not found");
+      return res.status(404).json('Post not found')
     }
 
     if (post.userId === req.body.userId || user.isAdmin) {
       // Check if the user is the owner of the post or an admin
-      await post.deleteOne();
-      res.status(200).json("Post has been deleted");
+      const publicId = extractPublicId(post.img)
+      if (publicId) {
+        await cloudinary.uploader.destroy(publicId)
+      }
+
+      await post.deleteOne()
+      res.status(200).json('Post has been deleted')
     } else {
-      res.status(403).json("You can delete only your post or you need admin privileges");
+      res
+        .status(403)
+        .json('You can delete only your post or you need admin privileges')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
+
 // Hide / Unhide a post
-router.put("/post/:id/hide", async (req, res) => {
+router.put('/post/:id/hide', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
-    const user = await User.findById(req.body.userId);
+    const post = await Post.findById(req.params.id)
+    const user = await User.findById(req.body.userId)
 
     if (!post) {
-      return res.status(404).json("Post not found");
+      return res.status(404).json('Post not found')
     }
 
     if (!user.isAdmin) {
-      return res.status(403).json("Only admin can hide/unhide posts");
+      return res.status(403).json('Only admin can hide/unhide posts')
     }
 
-    post.isHide = !post.isHide; // Toggle the value of isHide property
-    await post.save();
+    post.isHide = !post.isHide // Toggle the value of isHide property
+    await post.save()
 
     if (post.isHide) {
-      res.status(200).json("The post has been hidden");
+      res.status(200).json('The post has been hidden')
     } else {
-      res.status(200).json("The post has been unhidden");
+      res.status(200).json('The post has been unhidden')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
+
 //like / dislike a post
-
-router.put("/post/:id/like", async (req, res) => {
+router.put('/post/:id/like', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
+    const post = await Post.findById(req.params.id)
     if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json("The post has been liked");
+      await post.updateOne({ $push: { likes: req.body.userId } })
+      res.status(200).json('The post has been liked')
     } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("The post has been disliked");
+      await post.updateOne({ $pull: { likes: req.body.userId } })
+      res.status(200).json('The post has been disliked')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-//get a post
+})
 
-router.get("/post/:id", async (req, res) => {
+//get a post
+router.get('/post/:id', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
+    const post = await Post.findById(req.params.id)
+    res.status(200).json(post)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 //get timeline posts by userid
-
-router.get("/post/timeline/:userId", async (req, res) => {
+router.get('/post/timeline/:userId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.params.userId);
-    const userPosts = await Post.find({ userId: currentUser._id });
+    const currentUser = await User.findById(req.params.userId)
+    const userPosts = await Post.find({ userId: currentUser._id })
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
-        return Post.find({ userId: friendId });
+        return Post.find({ userId: friendId })
       })
-    );
-    res.status(200).json(userPosts.concat(...friendPosts));
+    )
+    res.status(200).json(userPosts.concat(...friendPosts))
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
+
 // get all timeline posts
-router.get("/post/all/timeline", async (req, res) => {
+router.get('/post/all/timeline', async (req, res) => {
   try {
-    const publicUsers = await User.find({ isPublic: true });
-    const publicUserIds = publicUsers.map(user => user._id);
+    const publicUsers = await User.find({ isPublic: true })
+    const publicUserIds = publicUsers.map((user) => user._id)
 
-    const timelinePosts = await Post.find({ userId: { $in: publicUserIds }, isHide: false });
-    res.status(200).json(timelinePosts);
+    const timelinePosts = await Post.find({
+      userId: { $in: publicUserIds },
+      isHide: false,
+    })
+    res.status(200).json(timelinePosts)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
+
 //get user profile post by userid
-
-router.get("/post/profile/:userId", async (req, res) => {
+router.get('/post/profile/:userId', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
-    const posts = await Post.find({ userId: user._id });
-    res.status(200).json(posts);
+    const user = await User.findById(req.params.userId)
+    const posts = await Post.find({ userId: user._id })
+    res.status(200).json(posts)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 // add comment to a post
-router.post("/post/:id/comment", async (req, res) => {
+router.post('/post/:id/comment', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
-    const user = await User.findById(req.body.userId);
+    const post = await Post.findById(req.params.id)
+    const user = await User.findById(req.body.userId)
 
     if (!post) {
-      return res.status(404).json("Post not found");
+      return res.status(404).json('Post not found')
     }
 
     const newComment = {
       userId: user._id,
       comment: req.body.comment,
-    };
+    }
 
-    post.comments.push(newComment);
-    await post.save();
+    post.comments.push(newComment)
+    await post.save()
 
-    res.status(200).json("Comment added successfully");
+    res.status(200).json('Comment added successfully')
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
+
 // Search posts by tags
-router.get("/post/search/tags", async (req, res) => {
+router.get('/post/search/tags', async (req, res) => {
   try {
-    const tags = req.query.tags.split(","); //http://localhost:8800/api/posts/search/tags?tags=tag1,tag2
-    const posts = await Post.find({ tags: { $all: tags } });
+    const tags = req.query.tags.split(',') //http://localhost:8800/api/posts/search/tags?tags=tag1,tag2
+    const posts = await Post.find({ tags: { $all: tags } })
 
     if (posts.length === 0) {
-      return res.status(404).json({ message: "No posts found for the specified tags." });
+      return res
+        .status(404)
+        .json({ message: 'No posts found for the specified tags.' })
     }
 
-    res.status(200).json(posts);
+    res.status(200).json(posts)
   } catch (err) {
-    res.status(500).json({ message: "An error occurred while searching for posts." });
+    res
+      .status(500)
+      .json({ message: 'An error occurred while searching for posts.' })
   }
-});
+})
 
 // Save or unsave a post
-router.put("/post/:id/save", async (req, res) => {
+router.put('/post/:id/save', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
-    const user = await User.findById(req.body.userId);
+    const post = await Post.findById(req.params.id)
+    const user = await User.findById(req.body.userId)
 
     if (!post) {
-      return res.status(404).json("Post not found");
+      return res.status(404).json('Post not found')
     }
 
-    const savedIndex = user.savedPosts.indexOf(post._id);
+    const savedIndex = user.savedPosts.indexOf(post._id)
 
     if (savedIndex === -1) {
       // Post is not saved, so save it
-      user.savedPosts.push(post._id);
-      await user.save();
-      res.status(200).json("Post has been saved");
+      user.savedPosts.push(post._id)
+      await user.save()
+      res.status(200).json('Post has been saved')
     } else {
       // Post is already saved, so unsave it
-      user.savedPosts.splice(savedIndex, 1);
-      await user.save();
-      res.status(200).json("Post has been unsaved");
+      user.savedPosts.splice(savedIndex, 1)
+      await user.save()
+      res.status(200).json('Post has been unsaved')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,275 +1,327 @@
-const User = require("../models/User");
-const router = require("express").Router();
-const bcrypt = require("bcrypt");
-const cors = require('cors');
-router.use(cors());
+import { v2 as cloudinary } from 'cloudinary'
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+})
+const User = require('../models/User')
+const router = require('express').Router()
+const bcrypt = require('bcrypt')
+const cors = require('cors')
+router.use(cors())
 
 // Update User
-router.put("/user/:id", async (req, res) => {
-  const { id } = req.params;
-  const { userId } = req.body;
+router.put('/user/:id', async (req, res) => {
+  const { id } = req.params
+  const { userId } = req.body
 
   try {
-    const user = await User.findById(userId);
+    const user = await User.findById(userId)
 
     if (!user) {
-      return res.status(404).json("User not found");
+      return res.status(404).json('User not found')
     }
 
     if (userId === id || user.isAdmin) {
       if (req.body.password) {
         try {
-          const salt = await bcrypt.genSalt(10);
-          req.body.password = await bcrypt.hash(req.body.password, salt);
+          const salt = await bcrypt.genSalt(10)
+          req.body.password = await bcrypt.hash(req.body.password, salt)
         } catch (err) {
-          return res.status(500).json(err);
+          return res.status(500).json(err)
         }
       }
 
       try {
-        await User.findByIdAndUpdate(id, { $set: req.body });
-        return res.status(200).json("Account has been updated");
+        await User.findByIdAndUpdate(id, { $set: req.body })
+        return res.status(200).json('Account has been updated')
       } catch (err) {
-        return res.status(500).json(err);
+        return res.status(500).json(err)
       }
     } else {
-      return res.status(403).json("You can update only your account or you must be an admin");
+      return res
+        .status(403)
+        .json('You can update only your account or you must be an admin')
     }
   } catch (err) {
-    return res.status(500).json(err);
+    return res.status(500).json(err)
   }
-});
-  
+})
 
-// Delete User
-router.delete("/user/:id", async (req, res) => {
-  const { id } = req.params;
-  const { userId } = req.body;
-
+// Update User Profile Picture
+router.put('/user/picture/:id', async (req, res) => {
   try {
-    const user = await User.findById(userId);
+    const userId = req.params.id
+    const user = await User.findById(userId)
 
     if (!user) {
-      return res.status(404).json("User not found");
+      return res.status(404).json({ message: 'User not found.' })
+    }
+
+    if (!req.body.img) {
+      return res.status(400).json({ message: 'No image file uploaded.' })
+    }
+
+    // Delete the previous profile picture from Cloudinary if needed
+    if (user.profilePicture) {
+      const publicId = user.profilePicture.split('/').pop().split('.')[0]
+      await cloudinary.uploader.destroy(publicId)
+    }
+
+    // Update the user's profile picture with the new Cloudinary URL
+    const imageURL = await cloudinary.uploader.upload(imgURL[0], {
+      upload_preset: 'unsigned_upload_posts',
+      public_id: `${userId}_profilePicture`,
+      allowed_formats: ['jpg', 'jpeg', 'png', 'svg', 'ico', 'webp'],
+    })
+    user.profilePicture = imageURL
+    await user.save()
+
+    res.status(200).json({
+      message: 'Profile picture updated successfully.',
+      profilePicture: req.file.path,
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      message: 'An error occurred while updating the profile picture.',
+    })
+  }
+})
+
+// Delete User
+router.delete('/user/:id', async (req, res) => {
+  const { id } = req.params
+  const { userId } = req.body
+
+  try {
+    const user = await User.findById(userId)
+
+    if (!user) {
+      return res.status(404).json('User not found')
     }
 
     if (userId === id || user.isAdmin) {
-      await User.findByIdAndDelete(id);
-      return res.status(200).json("Account has been deleted");
+      await User.findByIdAndDelete(id)
+      return res.status(200).json('Account has been deleted')
     } else {
-      return res.status(403).json("You can delete only your account or you must be an admin");
+      return res
+        .status(403)
+        .json('You can delete only your account or you must be an admin')
     }
   } catch (err) {
-    return res.status(500).json(err);
+    return res.status(500).json(err)
   }
-});
-  
-  
-  //get a user
-  router.get("/user/:id", async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      const { password, updatedAt, ...other } = user._doc;
-      res.status(200).json(other);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
+})
 
+//get a user
+router.get('/user/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const { password, updatedAt, ...other } = user._doc
+    res.status(200).json(other)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 // Follow a user
-router.put("/user/:id/follow", async (req, res) => {
-  const { userId } = req.body;
-  const { id } = req.params;
+router.put('/user/:id/follow', async (req, res) => {
+  const { userId } = req.body
+  const { id } = req.params
 
   if (userId !== id) {
     try {
-      const user = await User.findById(id);
-      const currentUser = await User.findById(userId);
+      const user = await User.findById(id)
+      const currentUser = await User.findById(userId)
 
       if (!user.followers.includes(userId)) {
         if (user.isPublic) {
           // If the account is public, follow the user directly
-          await user.updateOne({ $push: { followers: userId } });
-          await currentUser.updateOne({ $push: { followings: id } });
-          res.status(200).json("User has been followed");
+          await user.updateOne({ $push: { followers: userId } })
+          await currentUser.updateOne({ $push: { followings: id } })
+          res.status(200).json('User has been followed')
         } else {
           // If the account is private, send a follow request
           if (!user.followRequests.includes(userId)) {
             // Check if the user has already sent a follow request
-            await user.updateOne({ $push: { followRequests: userId } });
-            res.status(200).json("Follow request sent");
+            await user.updateOne({ $push: { followRequests: userId } })
+            res.status(200).json('Follow request sent')
           } else {
-            res.status(403).json("You have already sent a follow request");
+            res.status(403).json('You have already sent a follow request')
           }
         }
       } else {
-        res.status(403).json("You already follow this user");
+        res.status(403).json('You already follow this user')
       }
     } catch (err) {
-      res.status(500).json(err);
+      res.status(500).json(err)
     }
   } else {
-    res.status(403).json("You can't follow yourself");
+    res.status(403).json("You can't follow yourself")
   }
-});
+})
+
 // Accept follower request
-router.put("/user/:id/accept-follower", async (req, res) => {
-  const { followerId } = req.body;
-  const { id } = req.params;
+router.put('/user/:id/accept-follower', async (req, res) => {
+  const { followerId } = req.body
+  const { id } = req.params
 
   try {
-    const user = await User.findById(id);
+    const user = await User.findById(id)
 
     if (user.followRequests.includes(followerId)) {
       // Remove followerId from followRequests array
       user.followRequests = user.followRequests.filter(
         (request) => request.toString() !== followerId
-      );
+      )
 
       // Add followerId to followers array
-      user.followers.push(followerId);
+      user.followers.push(followerId)
 
-      await user.save();
+      await user.save()
 
-      res.status(200).json("Follower request accepted");
+      res.status(200).json('Follower request accepted')
     } else {
-      res.status(404).json("Follower request not found");
+      res.status(404).json('Follower request not found')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-  //unfollow a user
-  
-  router.put("/user/:id/unfollow", async (req, res) => {
-      if (req.body.userId !== req.params.id) {
-        try {
-          const user = await User.findById(req.params.id);
-          const currentUser = await User.findById(req.body.userId);
-          if (user.followers.includes(req.body.userId)) {
-            await user.updateOne({ $pull: { followers: req.body.userId } });
-            await currentUser.updateOne({ $pull: { followings: req.params.id } });
-            res.status(200).json("user has been unfollowed");
-          } else {
-            res.status(403).json("you dont follow this user");
-          }
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      } else {
-        res.status(403).json("you cant unfollow yourself");
-      }
-    });
-// Block a user
-router.put("/user/:id/block", async (req, res) => {
-  if (req.params.id !== req.body.userId) {
+})
+//unfollow a user
+
+router.put('/user/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
     try {
-      const userToBlock = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId);
-      if (!userToBlock.blockedUsers.includes(req.body.userId)) {
-        await userToBlock.updateOne({ $push: { blockedUsers: req.body.userId } });
-        res.status(200).json("User has been blocked");
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } })
+        await currentUser.updateOne({ $pull: { followings: req.params.id } })
+        res.status(200).json('user has been unfollowed')
       } else {
-        res.status(403).json("You have already blocked this user");
+        res.status(403).json('you dont follow this user')
       }
     } catch (err) {
-      res.status(500).json(err);
+      res.status(500).json(err)
     }
   } else {
-    res.status(403).json("You cannot block yourself");
+    res.status(403).json('you cant unfollow yourself')
   }
-});
+})
+// Block a user
+router.put('/user/:id/block', async (req, res) => {
+  if (req.params.id !== req.body.userId) {
+    try {
+      const userToBlock = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+      if (!userToBlock.blockedUsers.includes(req.body.userId)) {
+        await userToBlock.updateOne({
+          $push: { blockedUsers: req.body.userId },
+        })
+        res.status(200).json('User has been blocked')
+      } else {
+        res.status(403).json('You have already blocked this user')
+      }
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  } else {
+    res.status(403).json('You cannot block yourself')
+  }
+})
 
 // Unblock a user
-router.put("/user/:id/unblock", async (req, res) => {
+router.put('/user/:id/unblock', async (req, res) => {
   try {
-    const userToUnblock = await User.findById(req.params.id);
-    const currentUser = await User.findById(req.body.userId);
+    const userToUnblock = await User.findById(req.params.id)
+    const currentUser = await User.findById(req.body.userId)
     if (userToUnblock.blockedUsers.includes(req.body.userId)) {
-      await userToUnblock.updateOne({ $pull: { blockedUsers: req.body.userId } });
-      res.status(200).json("User has been unblocked");
+      await userToUnblock.updateOne({
+        $pull: { blockedUsers: req.body.userId },
+      })
+      res.status(200).json('User has been unblocked')
     } else {
-      res.status(403).json("You have not blocked this user");
+      res.status(403).json('You have not blocked this user')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 // Set account as public or not
-router.put("/user/:id/public", async (req, res) => {
+router.put('/user/:id/public', async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id)
 
     if (!user) {
-      return res.status(404).json("User not found");
+      return res.status(404).json('User not found')
     }
 
-    const { isPublic } = req.body;
+    const { isPublic } = req.body
 
-    user.isPublic = isPublic;
-    await user.save();
+    user.isPublic = isPublic
+    await user.save()
 
     if (isPublic) {
-      res.status(200).json("Account has been set as public");
+      res.status(200).json('Account has been set as public')
     } else {
-      res.status(200).json("Account has been set as private");
+      res.status(200).json('Account has been set as private')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 // Suspend a user
-router.put("/user/:id/suspend", async (req, res) => {
+router.put('/user/:id/suspend', async (req, res) => {
   try {
-    const adminUser = await User.findById(req.body.userId);
+    const adminUser = await User.findById(req.body.userId)
     if (!adminUser) {
-      return res.status(404).json("Admin user not found");
+      return res.status(404).json('Admin user not found')
     }
 
     if (adminUser.isAdmin) {
-      const user = await User.findById(req.params.id);
+      const user = await User.findById(req.params.id)
       if (!user) {
-        return res.status(404).json("User not found");
+        return res.status(404).json('User not found')
       }
 
-      user.suspended = req.body.suspended;
-      user.suspensionReason = req.body.suspensionReason;
-      await user.save();
+      user.suspended = req.body.suspended
+      user.suspensionReason = req.body.suspensionReason
+      await user.save()
 
-      res.status(200).json("User has been suspended");
+      res.status(200).json('User has been suspended')
     } else {
-      res.status(403).json("Only admins can suspend users");
+      res.status(403).json('Only admins can suspend users')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 // Unsuspend a user
-router.put("/user/:id/unsuspend", async (req, res) => {
+router.put('/user/:id/unsuspend', async (req, res) => {
   try {
-    const adminUser = await User.findById(req.body.userId);
+    const adminUser = await User.findById(req.body.userId)
     if (!adminUser) {
-      return res.status(404).json("Admin user not found");
+      return res.status(404).json('Admin user not found')
     }
 
     if (adminUser.isAdmin) {
-      const user = await User.findById(req.params.id);
+      const user = await User.findById(req.params.id)
       if (!user) {
-        return res.status(404).json("User not found");
+        return res.status(404).json('User not found')
       }
 
-      user.suspended = false;
-      user.suspensionReason = "";
-      await user.save();
+      user.suspended = false
+      user.suspensionReason = ''
+      await user.save()
 
-      res.status(200).json("User has been unsuspended");
+      res.status(200).json('User has been unsuspended')
     } else {
-      res.status(403).json("Only admins can unsuspend users");
+      res.status(403).json('Only admins can unsuspend users')
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
-module.exports = router;
+})
+module.exports = router
