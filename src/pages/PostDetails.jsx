1f4a553c9fac13cf61c99f5832fa9fa4d8c94f3b
ronaldding <ronaldding2002@gmail.com>
2024-04-11 11:32:39 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/pages/PostDetails.jsx

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -13,6 +13,7 @@ const PostDetails = () => {
   // const currentUserId = '660970232846199a041ae117'; 
   const [postContent, setPostContent] = useState([]);
   const [replyText, setReplyText] = useState('');
+  const [retweetText, setRetweetText] = useState('');
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -32,6 +33,9 @@ const PostDetails = () => {
   const handleReplyTextChange = (event) => {
     setReplyText(event.target.value);
   };
+  const handleRetweetTextChange = (event) => {
+    setRetweetText(event.target.value);
+  };
 
   // Function to handle replying to the post
   const handleReply = async () => {
@@ -50,6 +54,24 @@ const PostDetails = () => {
       console.error(error);
     }
   };
+
+  const handleRetweet = async () => {
+    try {
+      const commentData = {
+        userId: currentUserId,
+        desc: replyText
+      };
+
+      const res = await axios.post(`http://localhost:8800/retweet/${id}`, commentData);
+      console.log(res.data); // Assuming the server responds with the newly created comment
+
+      // Clear the reply text input
+      setReplyText('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 
   return (
     <div className="postDetails">
@@ -73,6 +95,27 @@ const PostDetails = () => {
             <div className="replyRight">
               <div className="postBtn" onClick={handleReply}>
                 <label>Reply</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="reply">
+          <div className="replyContainer">
+            <div className="replyLeft">
+              <img className='postProfileImg' src="/assets/person/1.jpeg" alt="Profile" />
+              <div className="replyText">
+                <input
+                  type="text"
+                  className="replyInput"
+                  placeholder="Tweet a reply"
+                  value={replyText}
+                  onChange={handleRetweetTextChange}
+                />
+              </div>
+            </div>
+            <div className="replyRight">
+              <div className="postBtn" onClick={handleRetweet}>
+                <label>Retweet</label>
               </div>
             </div>
           </div>
